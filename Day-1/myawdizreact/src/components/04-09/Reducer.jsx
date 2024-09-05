7423c9fcdd7bdfac9d8d8915a20a004d69af1e6d
Rahul choudhary <rgodara152001@gmail.com>
2024-09-05 09:51:37 +0530

    commit

--- Day-1/myawdizreact/src/components/04-09/Reducer.jsx
+++ Day-1/myawdizreact/src/components/04-09/Reducer.jsx
@@ -1,51 +1,49 @@
-import React, { useReducer } from 'react'
-import "../css/Navbar.css"
+import React, { useReducer } from "react";
+import "../css/Navbar.css";
 
-function Reducerfun(state , action ) {
-        switch(action.type) {
-            case  "Increment" : 
-            return { counter : state.counter +  1 }
-            case  "Decrement" :  
-            return { counter : state.counter -  1 } 
-            case  "Reset" : 
-            return  { counter : 0} 
-        }
-
-
+function Reducerfun(state, action) { // 1
+   
+  switch (action.type) {
+    case "Increment":
+      return { counter: state.counter + 1 };
+    case "Decrement":
+      return { counter: state.counter - 1 };
+    case "Reset":
+      return { counter: 0 };
+  }
 }
 
-const InitialState = {counter : 1};
+const InitialState = { counter: 1 }; // 2
 
 const Reducer = () => {
+  const [state, dispatch] = useReducer(Reducerfun, InitialState); // 3
 
-    const [state , dispatch] = useReducer(Reducerfun , InitialState);
+  function Increment() {
+    dispatch({ type: "Increment" });
+  }
 
-
-    function Increment() {
-      dispatch({type : "Increment"})
+  function Decrement() {
+    if (state.counter > 0) {
+      dispatch({ type: "Decrement" });
     }
+  }
 
-    function Decrement() {
-        if (state.counter > 0){
-        dispatch({type : "Decrement"})
-        }
-      }
-
-      function Reset() {
-        dispatch({type : "Reset"})
-      }
+  function Reset() {
+    dispatch({ type: "Reset" });
+  }
 
   return (
-    <div className='ParentReducer'>
-          <div>
-            <h1>Counter : {state.counter} </h1></div>
-          <div>
-          <button onClick={Increment}>+</button>
-          <button onClick={Decrement}>-</button>
-          <button onClick={Reset}>Reset</button>
-          </div>
+    <div className="ParentReducer">
+      <div>
+        <h1>Counter : {state.counter} </h1>
+      </div>
+      <div>
+        <button onClick={Increment}>+</button>
+        <button onClick={Decrement}>-</button>
+        <button onClick={Reset}>Reset</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Reducer
+export default Reducer;
