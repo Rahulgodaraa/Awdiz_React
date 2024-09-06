c7cac66f55165fb626095b9103b6b745f27db77f
Rahul choudhary <rgodara152001@gmail.com>
2024-09-06 22:15:24 +0530

    update

--- Day-1/myawdizreact/src/components/05-09/DarkModeContext.jsx
+++ Day-1/myawdizreact/src/components/05-09/DarkModeContext.jsx
@@ -1,3 +1,23 @@
+import { createContext, useReducer } from "react";
 
+const DarkModeContext = createContext();
 
- 
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case "Darkmode":
+      return { mode: "dark" };
+    case "LightMode":
+      return { mode: "light" };
+    default:
+      return { mode: "light" };
+  }
+};
+
+const InitialState = { mode } ;
+
+function ParentDarkModeComponent( { children } ){
+    const [state , dispatch] = useReducer(Reducer , InitialState);
+    <DarkModeContext.Provider value = {{state , dispatch}}>
+        {children}
+    </DarkModeContext.Provider>
+}
